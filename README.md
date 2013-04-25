addce2a6890ce2ec509c54b0c8cf438a69b7a43b
mike-t <mickwalton@gmail.com>
2013-04-26 01:15:34 +1000

    Added javascript form validation, tidied up colour scheme and HTML.

--- README.md
+++ README.md
@@ -1,15 +1,22 @@
 TODO
 
 Round 1:
-* fix footer padding issue?!
 * tidy css, rename classes, removed what not used (is very large)
-* typograhpy
+* typography
 * several background images with timer and nav
+* add access keys and tab order to forms
+* javascript validation of form fields on key up
+* users ip in jaascript hidden field
+* disable on submit
 
 Round 2:
-* recent activity is a stream of video, reviews, photos, so an so went camping here...
+* recent activity is a stream of video, reviews, photos, so an so went cam
+* ping here...
 
   Visual
+  big logo top left over top of nav bar and content
   mud path from tent to footer
+  rope or canvas stitched edging around container, body background of bush
+  
 
 
--- js/main.js
+++ js/main.js
@@ -1,1 +1,97 @@
+/* ===================================================
+ * General Purpose JS Functions
+ * ===================================================
+ * 
+ * @author:		Mike Walton
+ * @updated:	25/04/2013
+ * @desc:		general purpose js functions for form
+ * 				validation, ui interactions etc.
+ * 
+ * ================================================== */
 
+/* ===================================================
+ * General runtime operations
+ * =================================================== */
+ 
+// if trim isn't implemented by the browser then teach it 
+if (!String.prototype.trim) {
+	String.prototype.trim = function() {
+		return this.replace(/^\s+|\s+$/g,'');
+	}
+}
+/* =================================================== */
+ 	
+
+/* ===================================================
+ * fieldIsValid function
+ * ===================================================
+ * @params:		type(String) - type of check, fieldid(String) - the id of the form field to check
+ * @returns: 	boolean
+ * @desc:		check a form field, types are...
+ * 		name: alpha numberic, apostrophe and dashes
+ * 		email: email validation to catch most errors without false positives
+ * 		length: 
+ * =================================================== */
+function fieldIsValid(type, fieldid) {
+	// define variables
+	var valid = false;
+	var msg = '';
+	
+	// Define regular expressions for form validation
+	var emailRegEx = /^[a-zA-Z0-9.!#$%&amp;'*+\-\/=?\^_`{|}~\-]+@[a-zA-Z0-9\-]+(?:\.[a-zA-Z0-9\-]+)*$/;
+	var	namesRegEx = /^[a-z0-9_\-']+$/i;
+	var	minLength = 8;
+	
+	// get value from field
+	var field_val = document.getElementById(fieldid).value;
+	
+	// trim whitespace from field
+	field_val = field_val.trim();
+	
+	// do test based on type requested
+	switch(type) {
+		case 'email':
+			// check email is likely valid 
+			(emailRegEx.test(field_var)) ? valid = true : showFormError(fieldid, 'Your email address does not appear to be valid, please try again.');
+			break;
+		case 'name':
+			// check name only contains alpha, dashses or apostrophes.
+			(namesRegEx.test(field_var)) ? valid = true : showFormError(fieldid, 'Your name does not appear to be valid, names may only contain alphabetic characters, apostrpohes or dashes. Please try again.');
+			break;
+		case 'password':
+			// password must be at least 8 characters else display an error
+			(field_val.len() > minLength) ? valid = true : showFormError(fieldid, 'Passwords must be at least 8 characters, please try again.');
+			break;
+	}
+		
+	return valid;
+}
+/* =================================================== */
+
+
+/* ===================================================
+ * showFormError function
+ * ===================================================
+ * @params:		fieldid(String) - the id of the form field to check, msg(String) - the form error to display
+ * @returns:	n.a
+ * @desc:		displays a formatted error to the user on the form 
+ * =================================================== */
+function showFormError(fieldid, msg) {
+	//set the message
+	document.getElementById('validation-error-message').innerHTML = '<strong>Error: </strong>' + msg;
+	// show the message
+	document.getElementById('validation-error-box').style.display = 'block';
+}
+
+/* ===================================================
+ * fieldIsEqual function
+ * ===================================================
+ * @params:		fieldid(String) - the first field
+ * @returns:	boolean
+ * @desc:		checks fields for equality
+ * =================================================== */
+function fieldIsEqual(fieldid1, fieldid2) {
+	// compare fields, return true if the same
+	(document.getElementById(fieldid1).value == document.getElementById(fieldid2).value) ? return true : return false;
+}
+/* =================================================== */
